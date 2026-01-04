673509a337744e80d1b202be7ee2f2f6327bd36e
Danilo Lemes <13068064+Danil0v3s@users.noreply.github.com>
2026-01-04 21:53:05 +0000

    Split up mage skills (#9548)
    Co-authored-by: Lemongrass3110 <lemongrass@kstp.at>

--- src/map/skills/archer/chargearrow.cpp
+++ src/map/skills/archer/chargearrow.cpp
@@ -9,7 +9,7 @@ SkillChargeArrow::SkillChargeArrow() : WeaponSkillImpl(AC_CHARGEARROW)
 {
 }
 
-void SkillChargeArrow::calculateSkillRatio(const Damage *wd, const block_list *src, const block_list *target, uint16 skill_lv, int32 &base_skillratio) const
+void SkillChargeArrow::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const
 {
 	const map_session_data *sd = BL_CAST(BL_PC, src);
 	base_skillratio += 50;
