6546826124221ce78604489656b61f8a6c8f7f37
Meldrick <meldrickbech@gmail.com>
2026-03-10 17:13:11 -0300

    Fixed calculateSkillRatio parameters

--- src/map/skills/thief/sandattack.cpp
+++ src/map/skills/thief/sandattack.cpp
@@ -10,7 +10,7 @@
 SkillSandAttack::SkillSandAttack() : SkillImplRecursiveDamageSplash(TF_SPRINKLESAND) {
 }
 
-void SkillSandAttack::calculateSkillRatio(const Damage *wd, const block_list *src, const block_list *target, uint16 skill_lv, int32 &base_skillratio) const {
+void SkillSandAttack::calculateSkillRatio(const Damage* wd, const block_list* src, const block_list* target, uint16 skill_lv, int32& base_skillratio, int32 mflag) const {
 	base_skillratio += -30 + 30 * skill_lv;
 }
 
